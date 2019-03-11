7d83bae95f01c25058b633266610a124971850c6
Verthon <krzysiekksordyl@gmail.com>
2019-03-11 11:08:40 +0100

    Added webpack config, support for fonts, sass, es6, new colors

--- src/js/elements.js
+++
@@ -1,9 +0,0 @@
-
-export const temperature = document.getElementById('temperature');
-export const cityName = document.getElementById('city-name');
-export const weatherIcon = document.getElementById('weather-icon');
-
-export const iconDesc = document.getElementById('icon-desc');
-export const temperature = document.getElementById('temperature');
-export const wind = document.getElementById('wind');
-export const humidity = document.getElementById('humidity');
